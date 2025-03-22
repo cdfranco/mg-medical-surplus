14260ea45a0fa0aef675d49f7910c06bf6514e04
Caleb Franco <cdfranco@gmail.com>
2025-03-22 11:45:16 -0400

    Change menu button text to 'Contact Us' and revert product buttons to 'Request Quote'

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
                   to='/contact'
                   className='inline-flex items-center px-4 py-2 border-2 border-primary text-sm font-medium rounded-lg text-primary hover:bg-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary transition-all duration-300 ml-3'
                 >
-                  <span>Request Quote</span>
+                  <span>Contact Us</span>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     className='h-4 w-4 ml-2'
@@ -178,7 +178,7 @@ function App() {
                   className='block px-3 py-2 rounded-md text-base font-medium text-primary border border-primary hover:bg-primary hover:text-white'
                   onClick={() => setMobileMenuOpen(false)}
                 >
-                  Request Quote
+                  Contact Us
                 </Link>
               </div>
             </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -26,7 +26,7 @@ const ProductCard = React.memo(({ item, formatPrice }) => (
         state={{ equipmentType: item.name }}
         className='mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-dark'
       >
-        Contact Us
+        Request Quote
       </Link>
     </div>
   </div>
